7cea8c9d99833d92d512e4efaabca02ca8b57c0a
yogii006 <128319693+yogii006@users.noreply.github.com>
2023-10-20 00:52:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ input_data_as_numpy_array= np.asarray(input_data)
 input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
 prediction = new_model.predict(input_data_reshaped)
-print(prediction)
+st.write(prediction)
 
 if (prediction[0]== 0):
   print('The Person does not have a Heart Disease')
