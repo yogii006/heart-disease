da4124fdcad9dc9868a4ad4714e8960cf77d0911
yogii006 <128319693+yogii006@users.noreply.github.com>
2023-10-20 00:31:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+
+import streamlit as st
+import sklearn
+import pickle
+import numpy as np
+import pandas as pd
+
+
+st.header("Yogesh Yadav")
+new_model = pickle.load(open('model1.pkl','rb'))
+
+
+input_data = (62,0,0,140,268,0,0,160,0,3.6,0,2,2)
+
+# change the input data to a numpy array
+input_data_as_numpy_array= np.asarray(input_data)
+
+# reshape the numpy array as we are predicting for only on instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = new_model.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0]== 0):
+  print('The Person does not have a Heart Disease')
+else:
+  print('The Person has Heart Disease')
